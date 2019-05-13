2af9c775d5ff417e534847eef37c00c92b3cbb5f
Razvan Bugoi <razvanbugoi@gmail.com>
2019-05-13 16:45:05 +0100

    Renamed most ID's to avoid conflicts

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,21 +1,42 @@
-const inpTitle = document.getElementById("inpTitle");
-const inpShortDescription = document.getElementById("inpShortDescription");
-const inpSubmit = document.getElementById("inpSubmit");
-const lsOutput = document.getElementById("lsOutput");
+const movieTitle = document.getElementById("movieTitle");
+const movieDescription = document.getElementById("movieDescription");
+const submit = document.getElementById("submit");
+const movieRating = document.getElementById("movieRating");
+const checkBoxes = document.getElementById("checkBoxes");
+const published = document.getElementById("published");
+const output = document.getElementById("output");
+localStorage.setItem("obj" , JSON.stringify(obj));
+const toJSON = JSON.parse(localStorage.getItem("obj"));
 
-inpSubmit.onclick = function() {
-    const title = inpTitle.value;
-    const shortDescription = inpShortDescription.value;
+submit.onclick = function() {
+    const key = movieTitle.value;
+    const value = movieDescription.value;
 
-    if (title && shortDescription) {
-        localStorage.setItem(title, shortDescription);
+    if (key && value) {
+        localStorage.setItem(movieTitle.value, movieDescription.value, "obj" , JSON.stringify(obj));
         location.reload();
     }
+    console.log(localStorage);
 };
 
-for (let i = 0; i < localStorage.length; i += 1) {
+    for (let i = 0; i < localStorage.length; i += 1) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
 
     lsOutput.innerHTML += `${key}: ${value}<br />`;
 }
+
+const obj = {
+    genres : checkBoxes,
+    movieRating : movieRating,
+    published : published
+};
+
+// localStorage.setItem("obj" , JSON.stringify(obj));
+// const toJSON = JSON.parse(localStorage.getItem("obj"));
+// console.log(toJSON);
+
+
+
+// localStorage.setItem("obj", JSON.stringify({hi : "there"}));
+// console.log(JSON.parse(localStorage.getItem("obj")));
