1b435498407c8855081a41196bfa338f2983775e
Razvan Bugoi <razvanbugoi@gmail.com>
2019-05-15 16:42:41 +0100

    Finished script.js

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -10,67 +10,94 @@ const output = document.getElementById("output");
 
 submit.onclick = function() {
     let arr = [];
+
+    if (action.checked) {
+        arr.push("Action");
+    } 
+    if (adventure.checked) {
+        arr.push("Adventure");
+    }
+    if (comedy.checked) {
+        arr.push("Comedy");
+    } 
+    if (crime.checked) {
+        arr.push("Crime");
+    }
+    if (drama.checked) {
+        arr.push("Drama");
+    }
+    if (fantasy.checked) {
+        arr.push("Fantasy");
+    }
+    if (historical.checked) {
+        arr.push("Historical");
+    }
+    if (historicalFiction.checked) {
+        arr.push("Historical Fiction");
+    }
+    if (horror.checked) {
+        arr.push("Horror");
+    }
+    if (magicRealism.checked) {
+        arr.push("Magic Realism");
+    }
+    if (mystery.checked) {
+        arr.push("Mystery");
+    }
+    if (paranoidFiction.checked) {
+        arr.push("Paranoid Fiction");
+    }
+    if (philosophical.checked) {
+        arr.push("Philosophical");
+    }
+    if (political.checked) {
+        arr.push("Political");
+    }
+    if (romance.checked) {
+        arr.push("Romance");
+    }
+    if (saga.checked) {
+        arr.push("Saga");
+    }
+    if (satire.checked) {
+        arr.push("Satire");
+    }
+    if (scienceFiction.checked) {
+        arr.push("Science Fiction");
+    }
+    if (social.checked) {
+        arr.push("Social");
+    }
+    if (speculative.checked) {
+        arr.push("Speculative");
+    }
+    if (thriller.checked) {
+        arr.push("Thriller");
+    }
+    if (urban.checked) {
+        arr.push("Urban");
+    }
+    if (western.checked) {
+        arr.push("Western");
+    }
+
+
+
+
     const obj = {
-        title : movieTitle,
-        description : movieDescription,
-        // genres : checkbox, <- aici imi da o eroare in consola.
-        movieRating : movieRating,
-        published : published
+        title : movieTitle.value,
+        description : movieDescription.value,
+        genres : arr,
+        movieRating : movieRating.value,
+        published : published.value
     };
 
     if (movieTitle.value && movieDescription.value) {
         localStorage.setItem(movieTitle.value, JSON.stringify(obj));
         location.reload();
-    } else if (action.checked) {
-        arr.push("Action");
-    } else if (adventure.checked) {
-        arr.push("Adventure");
-    } else if (comedy.checked) {
-        arr.push("Comedy");
-    } else if (crime.checked) {
-        arr.push("Crime");
-    } else if (drama.checked) {
-        arr.push("Drama");
-    } else if (fantasy.checked) {
-        arr.push("Fantasy");
-    } else if (historical.checked) {
-        arr.push("Historical");
-    } else if (historicalFiction.checked) {
-        arr.push("Historical Fiction");
-    } else if (horror.checked) {
-        arr.push("Horror");
-    } else if (magicRealism.checked) {
-        arr.push("Magic Realism");
-    } else if (mystery.checked) {
-        arr.push("Mystery");
-    } else if (paranoidFiction.checked) {
-        arr.push("Paranoid Fiction");
-    } else if (philosophical.checked) {
-        arr.push("Philosophical");
-    } else if (political.checked) {
-        arr.push("Political");
-    } else if (romance.checked) {
-        arr.push("Romance");
-    } else if (saga.checked) {
-        arr.push("Saga");
-    } else if (satire.checked) {
-        arr.push("Satire");
-    } else if (scienceFiction.checked) {
-        arr.push("Science Fiction");
-    } else if (social.checked) {
-        arr.push("Social");
-    } else if (speculative.checked) {
-        arr.push("Speculative");
-    } else if (thriller.checked) {
-        arr.push("Thriller");
-    } else if (urban.checked) {
-        arr.push("Urban");
-    } else if (western.checked) {
-        arr.push("Western");
-    }
+    } 
     
     
-    // console.log(localStorage);
 
 };
 
@@ -78,23 +105,8 @@ submit.onclick = function() {
      const key = localStorage.key(i);
      const value = localStorage.getItem(key);
 
-    JSON.parse(localStorage.getItem('obj')).genres; 
+    
 
-     output.innerHTML += `${key}: ${value}<br />`;
+     output.innerHTML += `${key}: <b>description:</b> ${JSON.parse(value).description} ${JSON.parse(value).genres} ${JSON.parse(value).movieRating} ${JSON.parse(value).published}<br />`;
 }
 
-
-// localStorage.setItem("obj" , JSON.stringify(obj));
-// const toJSON = JSON.parse(localStorage.getItem("obj"));
-// console.log(toJSON);
-
-
-
-// localStorage.setItem("obj", JSON.stringify({hi : "there"}));
-// console.log(JSON.parse(localStorage.getItem("obj")));
-
-
-// definesti un array gol unde stochezi toate stringurile genres pentru un film (verifici cu checked fiecare id si il adaugi in array)
-// pregatesti obiectiul cu title, short description, genres, rating, release date
-// adaugi in localstorage un key si obiectul cu json stringify
-// in for loop parsezi propietatile din obiectul tau cu JSON.parse(localStorage.getItem('obj')).numeleProprietatiiTale
