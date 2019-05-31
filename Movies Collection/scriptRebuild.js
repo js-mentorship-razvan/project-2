50387579560fe9b0b869f82d229d7037f6fcc8bf
RazvanBugoi <46108577+RazvanBugoi@users.noreply.github.com>
2019-05-31 15:03:17 +0100

    Trying different design with Bootstrap

---
+++ Movies Collection/scriptRebuild.js
@@ -0,0 +1,40 @@
+const movieTitle = document.getElementById("movieTitle");
+const movieDescription = document.getElementById("movieDescription");
+const genres = document.getElementById("checkbox");
+const submit = document.getElementById("submit");
+const movieRating = document.getElementById("movieRating");
+const published = document.getElementById("published");
+const output = document.getElementById("output");
+
+
+
+submit.onclick = function () {
+    let arr = [];
+    let options = document.querySelectorAll(".custom-control-input");
+    arr.push.apply(arr, [...options].reduce((prev, elem) => ((elem.checked && prev.push(elem.value)), prev), []));
+
+    const obj = {
+        title: movieTitle.value,
+        description: movieDescription.value,
+        genres: arr,
+        movieRating: movieRating.value,
+        published: published.value
+    };
+
+    if (movieTitle.value && movieDescription.value) {
+        localStorage.setItem(movieTitle.value, JSON.stringify(obj));
+        location.reload();
+    }
+};
+
+for (let i = 0; i < localStorage.length; i += 1) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    output.innerHTML += ` <li> ${key}: <b>description:</b> ${JSON.parse(value).description} ${JSON.parse(value).genres} ${JSON.parse(value).movieRating} ${JSON.parse(value).published} <button id="${key}" onclick="deleteMovie(this);">Delete</button> <br /></li>`;
+}
+
+function deleteMovie(currentEl) {
+    localStorage.removeItem(currentEl.getAttribute('id'));
+    location.reload();
+}
